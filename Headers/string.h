3dee8b394aaf42fc4cc71544d8c6d8d25f53dac2
Eloquence4 <zelin11@live.com>
2016-12-09 22:49:12 +0200

    Add my string implementation

---
+++ Headers/string.h
@@ -0,0 +1,89 @@
+﻿/**
+ *  
+ * Solution to homework task
+ * Data Structures Course
+ * Faculty of Mathematics and Informatics of Sofia University
+ * Winter semester 2016/2017
+ *
+ * @author Ilian Georgiev
+ * @idnumber 45134
+ * @task 1
+ * @compiler VC
+ *
+ */
+#ifndef StringDef
+#define StringDef
+#include <iostream>
+
+enum STRING_ERRORS
+{
+	ILLEGAL_INDEX,
+	STRING_IS_EMPTY
+};
+
+class string
+{
+public:
+	////////////// G4
+	string();                             // O(1)
+	~string();                            // O(1)
+	string& operator=(const string& rhs); // O(n), O(1) if this == &rhs
+	string(const string& source);         // O(n)
+	/////////////////
+
+	string(const char* initText); // O(n)
+
+	size_t size() const;    // O(1), Returns max string length
+	size_t length() const;  // O(1), Returns the string length
+	size_t max_size() const;// O(1), Returns max string size before having to resize
+
+	void resize(size_t targetSize); // O(n), Resizes the string
+	void reserve(size_t howMuch);   // O(n), Resizes the string to the current size + howMuch
+
+	bool empty() const; // O(1), Check if empty
+
+	char& operator[](int index);		// O(1), Unstable, throws ILLEGAL_INDEX
+	char& operator[](size_t index);		// O(1), Unstable, throws ILLEGAL_INDEX
+	char operator[](int index) const;	// O(1), Unstable, throws ILLEGAL_INDEX
+	char operator[](size_t index) const;// O(1), Unstable, throws ILLEGAL_INDEX
+	char& at(int index);				// O(1), Unstable, throws ILLEGAL_INDEX
+	char& at(size_t index);				// O(1), Unstable, throws ILLEGAL_INDEX
+	char at(int index) const;			// O(1), Unstable, throws ILLEGAL_INDEX
+	char at(size_t index) const;		// O(1), Unstable, throws ILLEGAL_INDEX
+
+	char& front();		// O(1), unstable, throws STRING_IS_EMPTY
+	char& back();		// O(1), unstable, throws STRING_IS_EMPTY
+	char front() const;	// O(1), unstable, throws STRING_IS_EMPTY
+	char back() const;	// O(1), unstable, throws STRING_IS_EMPTY
+
+	string& operator+=(const string& rhs);		// O(n) or O(2n) if no additional space is reserved
+	string operator+(const string& rhs) const;	// O(n) or O(2n) if no additional space is reserved
+	void append(const string& rhs);				// O(1) or O(n) if no additional space is reserved
+
+	string& operator+=(const char* rhs);		// O(n) or O(2n) if no additional space is reserved
+	string operator+(const char* rhs) const;	// O(n) or O(2n) if no additional space is reserved
+	void append(const char* rhs);				// O(1) or O(n) if no additional space is reserved
+
+	string& operator+=(char rhs);		// O(1) or O(n) if no additional space is reserved
+	string operator+(char rhs) const;	// O(1) or O(n) if no additional space is reserved
+	void append(char rhs);				// O(1) or O(n) if no additional space is reserved
+
+	bool operator==(const string& rhs) const; // O(n)
+	bool operator!=(const string& rhs) const; // O(n)
+
+	operator const char*() const; // O(1)
+
+	void clear(); // O(1)
+	void shrink();// O(n)
+
+private:
+
+	void delStr(); // O(1)
+	void copy(const char* newText, size_t newMaxSize); // O(n)
+
+	char* text;
+	size_t maxSize;
+	size_t curSize;
+};
+
+#endif // StringDef
---
+++ Source Files/string.cpp
@@ -0,0 +1,348 @@
+﻿/**
+ *  
+ * Solution to homework task
+ * Data Structures Course
+ * Faculty of Mathematics and Informatics of Sofia University
+ * Winter semester 2016/2017
+ *
+ * @author Ilian Georgiev
+ * @idnumber 45134
+ * @task 1
+ * @compiler VC
+ *
+ */
+#include <iostream>
+#include "string.h"
+
+/////////////// G4
+
+string::string()
+	: text(nullptr)
+	, maxSize(0)
+	, curSize(0)
+{
+}
+
+string::~string()
+{
+	delStr();
+}
+
+string& string::operator=(const string& rhs)
+{
+	if(this != &rhs)
+		copy(rhs.text, rhs.maxSize);
+	return *this;
+}
+
+string::string(const string & source)
+	: text(nullptr)
+	, maxSize(0)
+	, curSize(0)
+{
+	copy(source.text, source.maxSize);
+}
+
+/////////////// Public
+
+string::string(const char* initText)
+	: text(nullptr)
+	, maxSize(0)
+	, curSize(0)
+{
+	copy(initText, strlen(initText));
+}
+
+size_t string::size() const
+{
+	return curSize;
+}
+
+size_t string::length() const
+{
+	return curSize;
+}
+
+size_t string::max_size() const
+{
+	return maxSize;
+}
+
+void string::resize(size_t targetSize)
+{
+	if(targetSize == 0)
+	{
+		delete[] text;
+		maxSize = 0;
+		return;
+	}
+
+	char* newString = new char[targetSize+1];
+
+	if(!empty())
+	if(targetSize > maxSize)
+		for(size_t i = 0; i <= curSize; i++)
+			newString[i] = text[i];
+	else
+	{
+		for(size_t i = 0; i < targetSize-1; i++)
+			newString[i] = text[i];
+		newString[targetSize-1] = '\0';
+	}
+
+	maxSize = targetSize;
+	delete[] text;
+	text = newString;
+}
+
+void string::reserve(size_t howMuch)
+{
+	resize(maxSize + howMuch);
+}
+
+bool string::empty() const
+{
+	return maxSize == 0 || text[0] == '\0';
+}
+
+char& string::operator[](int index)
+{
+	return this->at(index);
+}
+
+char& string::operator[](size_t index)
+{
+	return this->at(index);
+}
+
+char string::operator[](int index) const
+{
+	return this->at(index);
+}
+
+char string::operator[](size_t index) const
+{
+	return this->at(index);
+}
+
+char& string::at(int index)
+{
+	if(index < 0)
+		throw ILLEGAL_INDEX;
+
+	return this->at((size_t) index);
+}
+
+char& string::at(size_t index)
+{
+	if(index >= curSize)
+		throw ILLEGAL_INDEX;
+	else
+		return text[index];
+}
+
+char string::at(int index) const
+{
+	if(index < 0)
+		throw ILLEGAL_INDEX;
+
+	return this->at((size_t)index);
+}
+
+char string::at(size_t index) const
+{
+	if(index >= curSize)
+		throw ILLEGAL_INDEX;
+	else
+		return text[index];
+}
+
+char& string::front()
+{
+	if(empty())
+		throw STRING_IS_EMPTY;
+
+	return text[0];
+}
+
+char& string::back()
+{
+	if(empty())
+		throw STRING_IS_EMPTY;
+
+	return text[curSize-1];
+}
+
+char string::front() const
+{
+	if(empty())
+		throw STRING_IS_EMPTY;
+
+	return text[0];
+}
+
+char string::back() const
+{
+	if(empty())
+		throw STRING_IS_EMPTY;
+
+	return text[curSize - 1];
+}
+
+string& string::operator+=(const string& rhs)
+{
+	size_t str1Len = curSize;
+	curSize += rhs.curSize;
+
+	if(maxSize <= curSize)
+		reserve(rhs.maxSize);
+
+	for(size_t i = str1Len; i <= curSize; i++)
+		text[i] = rhs.text[i - str1Len];
+
+	return *this;
+}
+
+string string::operator+(const string& rhs) const
+{
+	string newString(*this);
+
+	newString += rhs;
+
+	return newString;
+}
+
+void string::append(const string & rhs)
+{
+	(*this) += rhs;
+}
+
+string & string::operator+=(const char * rhs)
+{
+	size_t str1Len = curSize;
+	size_t str2Len = strlen(rhs);
+	curSize += str2Len;
+
+	if(maxSize <= curSize)
+		reserve(str2Len);
+
+	for(size_t i = str1Len; i <= curSize; i++)
+		text[i] = rhs[i - str1Len];
+
+	return *this;
+}
+
+string string::operator+(const char * rhs) const
+{
+	string newStr(*this);
+
+	newStr += rhs;
+
+	return newStr;
+}
+
+void string::append(const char * rhs)
+{
+	(*this) += rhs;
+}
+
+string& string::operator+=(char rhs)
+{
+	if(curSize >= maxSize)
+		reserve((size_t)25);
+
+	text[curSize] = rhs;
+	text[++curSize] = '\0';
+
+	return *this;
+}
+
+string string::operator+(char rhs) const
+{
+	string newStr(*this);
+
+	newStr += rhs;
+
+	return newStr;
+}
+
+void string::append(char rhs)
+{
+	(*this) += rhs;
+}
+
+bool string::operator==(const string& rhs) const
+{
+	int i = 0;
+
+	while(text[i] == rhs.text[i])
+	{
+		if(text[i] == '\0')
+			return true;
+		i++;
+	}
+	return false;
+}
+
+bool string::operator!=(const string& rhs) const
+{
+	return !(*this == rhs);
+}
+
+string::operator const char*() const
+{
+	return text;
+}
+
+void string::clear()
+{
+	if(maxSize != 0)
+		text[0] = '\0';
+	curSize = 0;
+}
+
+void string::shrink()
+{
+	if(maxSize == curSize)
+		return;
+
+	if(curSize == 0)
+	{
+		maxSize = 0;
+		delete[] text;
+	}
+
+	char* newText = new char[curSize];
+
+	for(size_t i = 0; i <= curSize; i++)
+		newText[i] = text[i];
+
+	delete[] text;
+	text = newText;
+
+	maxSize = curSize;
+}
+
+/////////////// Private
+
+void string::delStr()
+{
+	curSize = 0;
+	maxSize = 0;
+	delete[] text;
+}
+
+void string::copy(const char* newText, size_t newMaxSize)
+{
+	text = new char[newMaxSize+1];
+
+	int i;
+
+	for(i = 0; newText[i] != '\0'; i++)
+		text[i] = newText[i];
+
+	curSize = i;
+
+	text[i] = '\0';
+	maxSize = newMaxSize;
+}
